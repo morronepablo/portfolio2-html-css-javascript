d3125df6674c015102144bf3cf5472c326c9fc1a
Pablo Morrone <pmorrone@it.nortia.com.ar>
2022-12-26 12:08:20 -0300

    subiendo proyecto portfolio2

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/* ========================= typing animation ========================= */
+var typed = new Typed('.typing', {
+    strings: ["Web Desinger", "Web Developer", "Graphics Desinger", "YouTuber"],
+    typeSpeed: 100,
+    BackSpeed: 60,
+    loop: true
+})
